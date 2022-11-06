45267c9d2b3d5430ca82a17d54fe820aa38a218a
Gaurav Kumar <gauravk13101@gmail.com>
2022-11-06 20:27:56 +0530

    added duplicate number problem

---
+++ Arrays/duplicateNumber.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+    // Brute Force Method
+    int fansDuplicate(vector<int>& nums)
+    {
+           int ans = 0;
+		// sort the vector
+        sort(nums.begin(),nums.end());
+        for(int i = 0; i < nums.size() - 1; i++)
+        {
+            if(nums[i] == nums[i+1])
+            {
+                ans = nums[i];
+                break;
+            }
+        }
+        // return duplicate value
+        return ans;
+    }
+};
